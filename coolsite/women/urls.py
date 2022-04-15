7ee04ce80059732c00beb51c1e18ebfefed55ef8
Yan Alsheuski <60854775+Lavliet90@users.noreply.github.com>
2022-04-15 12:31:08 +0300

    Update urls.py
    cleaning

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -13,6 +13,4 @@ urlpatterns = [
     path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
     path('category/<slug:cat_slug>', WomenCategory.as_view(), name='category'),
 
-    # path('cats/<int:catid>/', categories),
-    # re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
 ]
